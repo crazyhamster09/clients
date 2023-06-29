4a6f241367283e3a2715ace34df00c6f6d3cc9df
crazyhamster09 <137649362+crazyhamster09@users.noreply.github.com>
2023-06-30 00:07:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,7 @@ def sidebar_input_features():
     gender = st.sidebar.selectbox("Пол", ("Мужской", "Женский"))
     age = st.sidebar.slider("Возраст", min_value=1, max_value=85, value=20,
                             step=1)
-    customer_type = st.sidebar.selectbox("Тип потребителя", (
-    "Лояльный клиент", "Нелояльный клиент"))
+    customer_type = st.sidebar.selectbox("Тип потребителя", ("Лояльный клиент", "Нелояльный клиент"))
     type_of_travel = st.sidebar.selectbox("Тип поездки", ("Деловая поездка", "Личная поездка"))
 
     class_ = st.sidebar.selectbox("Класс", ("Бизнес", "Эко", "Эко плюс"))
