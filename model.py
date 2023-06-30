da13fe8b9433fed2faccb6fa0314e6eace83c87a
crazyhamster09 <137649362+crazyhamster09@users.noreply.github.com>
2023-06-30 14:05:54 +0300

    Update model.py

--- model.py
+++ model.py
@@ -6,13 +6,9 @@ RANDOM_STATE = 42
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
 
 from pickle import dump, load
 
