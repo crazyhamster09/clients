21fd2a44f568017c14492bf652422965199b9b33
crazyhamster09 <137649362+crazyhamster09@users.noreply.github.com>
2023-06-30 15:18:52 +0300

    Update model_.py

--- model_.py
+++ model_.py
@@ -39,7 +39,7 @@ def preprocess_data(df: pd.DataFrame, test=True):
     else:
         X_df = df
 
-    to_encode = ['Gender']
+    to_encode = ['Gender', 'Customer Type', 'Type of Travel', 'Class']
     for col in to_encode:
         dummy = pd.get_dummies(X_df[col], prefix=col)
         X_df = pd.concat([X_df, dummy], axis=1)
