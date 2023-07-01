6237848ac9d29f2a8ec6c0183e9565f55b15c336
crazyhamster09 <137649362+crazyhamster09@users.noreply.github.com>
2023-07-01 08:26:29 +0300

    Update apptest.py

--- apptest.py
+++ apptest.py
@@ -3,6 +3,13 @@ import streamlit as st
 from PIL import Image
 
 image = Image.open('data/Без названия.jpg')
+st.set_page_config(
+        layout="wide",
+        initial_sidebar_state="auto",
+        page_title="Clients",
+        page_icon=image,
+
+    )
 st.write(
         """
         # Классификация удовлетворенности клиента полетом
