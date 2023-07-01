8959dbb6d66830695ad532eae49e0c4603412ae7
crazyhamster09 <137649362+crazyhamster09@users.noreply.github.com>
2023-07-01 09:44:10 +0300

    Update apptest.py

--- apptest.py
+++ apptest.py
@@ -26,17 +26,6 @@ def write_user_data(df):
         st.write("## Ваши данные")
         st.write(df)
 st.sidebar.header('Заданные пользователем параметры')
-user_input_df = sidebar_input_features()
-train_df = open_data()
-train_X_df, _ = split_data(train_df)
-full_X_df = pd.concat((user_input_df, train_X_df), axis=0)
-preprocessed_X_df = preprocess_data(full_X_df, test=False)
-
-user_X_df = preprocessed_X_df[:1]
-write_user_data(user_X_df)
-
-prediction, prediction_probas = load_model_and_predict(user_X_df)
-write_prediction(prediction, prediction_probas)
 
 def sidebar_input_features():
     gender = st.sidebar.selectbox("Пол", ("Мужской", "Женский"))
@@ -122,4 +111,18 @@ def sidebar_input_features():
     df = pd.DataFrame(data, index=[0])
 
     return df
+        
+user_input_df = sidebar_input_features()
+train_df = open_data()
+train_X_df, _ = split_data(train_df)
+full_X_df = pd.concat((user_input_df, train_X_df), axis=0)
+preprocessed_X_df = preprocess_data(full_X_df, test=False)
+
+user_X_df = preprocessed_X_df[:1]
+write_user_data(user_X_df)
+
+prediction, prediction_probas = load_model_and_predict(user_X_df)
+write_prediction(prediction, prediction_probas)
+
+
   
