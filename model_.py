1e9c04fbeb00e5e70238acc7c62c20307443293a
crazyhamster09 <137649362+crazyhamster09@users.noreply.github.com>
2023-07-01 22:24:18 +0300

    Update model_.py

--- model_.py
+++ model_.py
@@ -16,6 +16,10 @@ def open_data(path="https://raw.githubusercontent.com/evgpat/edu_stepik_from_ide
     df = pd.read_csv(path)
     return df
 
+def split_data(df: pd.DataFrame):
+    y = df['satisfaction']
+    X = df.drop(['satisfaction', 'id', 'Class'], axis=1)
+    return X, y
 
 def preprocess_data(df: pd.DataFrame, test=True):
     df.dropna(inplace=True)
@@ -24,39 +28,35 @@ def preprocess_data(df: pd.DataFrame, test=True):
     df = df[(df['Age'] > Q1-1.5*(Q3-Q1)) & (df['Age'] < Q3+1.5*(Q3-Q1))& (df['Flight Distance'] > Q1-1.5*(Q3-Q1)) & (df['Flight Distance'] < Q3+1.5*(Q3-Q1))&(df['Departure Delay in Minutes'] > Q1-1.5*(Q3-Q1)) & (df['Departure Delay in Minutes'] < Q3+1.5*(Q3-Q1)) & (df['Arrival Delay in Minutes'] > Q1-1.5*(Q3-Q1)) & (df['Arrival Delay in Minutes'] < Q3+1.5*(Q3-Q1))& (df['Departure/Arrival time convenient'] > Q1-1.5*(Q3-Q1)) & (df['Departure/Arrival time convenient'] < Q3+1.5*(Q3-Q1))]
     df = df[(df['Departure/Arrival time convenient'] <= 5)]
     df = df[(df['Inflight entertainment'] <= 5) & (df['Checkin service'] <= 5) &(df['Cleanliness'] <= 5)]
-    return df
 
-
-def split_data(df: pd.DataFrame):
-    y = df['satisfaction']
-    X = df.drop(['satisfaction', 'id', 'Class'], axis=1)
-    return X, y
-
-    train_examples = df['x_train']
-    train_labels = df['y_train']
-    test_examples = df['x_test']
-    test_labels = df['y_test']
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    if test:
+        X_df, y_df = split_data(df)
+    else:
+        X_df = df
 
     to_encode = ['Gender', 'Customer Type', 'Type of Travel']
     for col in to_encode:
-        dummy = pd.get_dummies(X[col], prefix=col)
-        X = pd.concat([X, dummy], axis=1)
-        X.drop(col, axis=1, inplace=True)
+        dummy = pd.get_dummies(X_df[col], prefix=col)
+        X_df = pd.concat([X_df, dummy], axis=1)
+        X_df.drop(col, axis=1, inplace=True)
 
+    if test:
+        return X_df, y_df
+    else:
+        return X_df
+
+    return X_df, y_df
     
-    return X, y
-    
 
 
 
 
-def fit_and_save_model(X):
+def fit_and_save_model(X_df, y_df):
     model = LogisticRegression()
-    model.fit(X, y)
-    pred = model.predict(X)
+    model.fit(X_df, y_df)
+    pred = model.predict(X_df)
 
-    accuracy = accuracy_score(test_prediction, y)
+    accuracy = accuracy_score(test_prediction, y_df)
     print(f"Model accuracy is {accuracy}")
 
     with open("data/model.pickle", "wb") as file:
@@ -65,14 +65,14 @@ def fit_and_save_model(X):
     print(f"Model was saved")
 
 
-def load_model_and_predict(X):
+def load_model_and_predict(df):
     with open("data/model.pickle", 'rb') as file:
         model = pickle.load(file)
 
-    prediction = model.predict(X)
+    prediction = model.predict(df)
     # prediction = np.squeeze(prediction)
 
-    prediction_proba = model.predict_proba(X)
+    prediction_proba = model.predict_proba(df)
     # prediction_proba = np.squeeze(prediction_proba)
 
 
@@ -98,5 +98,5 @@ def load_model_and_predict(X):
 
 if __name__ == "__main__":
     df = open_data()
-    X, y = preprocess_data(df)
-    fit_and_save_model(X, y)
+    X_df, y_df = preprocess_data(df)
+    fit_and_save_model(X_df, y_df)
