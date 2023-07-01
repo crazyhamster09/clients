1898f7c2ec6a5bd1309ddaac79828b1ddabf9be2
crazyhamster09 <137649362+crazyhamster09@users.noreply.github.com>
2023-07-01 19:54:24 +0300

    Update model_.py

--- model_.py
+++ model_.py
@@ -24,7 +24,7 @@ def preprocess_data(df: pd.DataFrame, test=True):
     df_preprocessed = df[(df['Age'] > Q1-1.5*(Q3-Q1)) & (df['Age'] < Q3+1.5*(Q3-Q1))& (df['Flight Distance'] > Q1-1.5*(Q3-Q1)) & (df['Flight Distance'] < Q3+1.5*(Q3-Q1))&(df['Departure Delay in Minutes'] > Q1-1.5*(Q3-Q1)) & (df['Departure Delay in Minutes'] < Q3+1.5*(Q3-Q1)) & (df['Arrival Delay in Minutes'] > Q1-1.5*(Q3-Q1)) & (df['Arrival Delay in Minutes'] < Q3+1.5*(Q3-Q1))& (df['Departure/Arrival time convenient'] > Q1-1.5*(Q3-Q1)) & (df['Departure/Arrival time convenient'] < Q3+1.5*(Q3-Q1))]
     df_preprocessed = df[(df['Departure/Arrival time convenient'] <= 5)]
     df_preprocessed = df[(df['Inflight entertainment'] <= 5) & (df['Checkin service'] <= 5) &(df['Cleanliness'] <= 5)]
-    df_preprocessed = df.drop(['id'])
+    df_preprocessed = df.drop(['id'], axis=1)
     return df_preprocessed
 
 
